65bb04c943dc5ee3f12950ce58324f56bf28cf1c
EderMar941706 <a_martinezeom@globalhitss.com>
2020-12-14 13:46:55 -0600

    baseprueba

---
+++ src/test/java/baseprueba.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class baseprueba {
+    private WebDriver driver;
+
+    public void setUp(){
+        System.setProperty("webdrive.chrome.drive", "resources/chromedrive.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+    public static void main(String args[]){
+        baseprueba test = new baseprueba();
+        test.setUp();
+    }
+}
